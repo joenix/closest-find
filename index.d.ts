dba242d89a5db044ee2efe89b94a28b1f9fad59a
joenix <joenix.com@gmail.com>
2022-01-19 14:31:40 +0800

    feat: update

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-declare const _default: (element: Element, selector: string, self: boolean) => void;
+declare const _default: (element: Element, selector: string, self?: boolean) => Element | undefined;
 export default _default;
